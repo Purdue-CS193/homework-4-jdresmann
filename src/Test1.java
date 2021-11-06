5aeeb9e836d5865aacf1af4f4fb0d70f0ec13ed7
jdresmann <89816033+jdresmann@users.noreply.github.com>
2021-11-05 21:32:46 -0400

    fixed typo + spacing errors

--- src/Test1.java
+++ src/Test1.java
@@ -1,25 +1,26 @@
-public static double basicCalculator(Operation operation, int num1, int num2){
+public class Test1 {
+    public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
 
 
         if (operation == Operation.ADD) {
-		result = num1 + num2;
-	}
-	else if (operation == Operation.SUBTRACT) {
-	result = num1 - num2;
-	}
-	else if (operation == Operation.MULTIPLY) {
-	result = num1 * num2;
-	}
-	else if (operation == Operation.DIVIDE) {
-		if (num2 == 0) {
-			System.out.println("Can't divide by 0");
-		} else {
-			result = ((double) num1) / num2;
-		}
-	}
+            result = num1 + num2;
+        }
+        else if (operation == Operation.SUBTRACT) {
+            result = num1 - num2;
+        }
+        else if (operation == Operation.MULTIPLY) {
+            result = num1 * num2;
+        }
+        else if (operation == Operation.DIVIDE) {
+            if (num2 == 0) {
+                System.out.println("Can't divide by 0");
+            } else {
+                result = ((double) num1) / num2;
+            }
+        }
 
-	return result;
+        return result;
     }
 
     public static void main(final String[] args) {
